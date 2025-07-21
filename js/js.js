7b1d25add8b777d8210556af8389e5d085cb0ff6
inyeong99 <123rkddlsdud@naver.com>
2025-07-21 11:24:14 +0900

    main 작업

--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-  console.log("jQuery 연결 성공!");
+// script.js 파일 내용
+//alert("JavaScript 파일이 연결되었습니다!");
+
+$(document).ready(function () {
+  $(".nav > ul > li").mouseover(function () {
+    $(this).find(".submenu").stop().slideDown(200);
+  });
+  $(".nav > ul > li").mouseout(function () {
+    $(this).find(".submenu").stop().slideUp(200);
+  });
 });
