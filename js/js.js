eeeb43fe23d6777f047eade62acdd43020b96d7a
inyeong99 <123rkddlsdud@naver.com>
2025-07-22 17:00:47 +0900

    no message

--- js/js.js
+++ js/js.js
@@ -4,28 +4,52 @@
 // 네비
 $(document).ready(function () {
   // 메뉴 열기
-  $(".nav > ul > li").on("mouseenter focusin", function () {
-    $(this).find(".submenu").stop(true, true).slideDown(200);
-    $(this).find("> a").attr("aria-expanded", "true");
+  let $menuLi = $(".nav > ul > li"),
+    $depth1Anchor = $menuLi.find("> a");
+
+  $depth1Anchor.on("mouseenter focus", function () {
+    $menuLi.find(".submenu").stop(true, true).slideUp(200);
+    $depth1Anchor.attr("aria-expanded", "false");
+    $(this).closest("li").find(".submenu").stop(true, true).slideDown(200);
+    $(this).attr("aria-expanded", "true");
   });
 
   // 메뉴 닫기
-  $(".nav > ul > li").on("mouseleave focusout", function () {
-    const $li = $(this);
-    setTimeout(function () {
-      if (!$li.find(":focus").length) {
-        $li.find(".submenu").stop(true, true).slideUp(200);
-        $li.find("> a").attr("aria-expanded", "false");
-      }
-    }, 100);
+  $menuLi.on("mouseleave", function () {
+    $(this).find(".submenu").stop(true, true).slideUp(200);
+    $(this).find("> a").attr("aria-expanded", "false");
   });
 
-  $(".submenu a").on("keydown", function (e) {
-    if (e.key === "Escape") {
-      const $parent = $(this).closest("li");
-      $parent.find(".submenu").slideUp(200);
-      $parent.find("> a").attr("aria-expanded", "false").focus();
+  $menuLi.find(".submenu a").on("blur", function () {
+    setTimeout(() => {
+      if (!$(this).closest("li").find(":focus").length) {
+        const $parentLi = $(this).closest("li");
+        $parentLi.find(".submenu").stop(true, true).slideUp(200);
+        $parentLi.find("> a").attr("aria-expanded", "false");
+      }
+    }, 10);
+  });
+
+  const $mainnav = $(".mainnav");
+  $mainnav.on("keydown", function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+      e.preventDefault();
+      $mainnav.find(".submenu").slideUp(200);
+      $mainnav
+        .find("> ul > li > a[aria-expanded='true']")
+        .attr("aria-expanded", "false");
     }
+  });
+
+  $mainnav.on("focusout", function (e) {
+    setTimeout(function () {
+      const activeEl = document.activeElement;
+      if (!$mainnav[0].contains(activeEl)) {
+        // 포커스가 mainnav 바깥으로 나갔으면 메뉴 닫기
+        $mainnav.find(".submenu").slideUp(200);
+        $mainnav.find("> ul > li > a").attr("aria-expanded", "false");
+      }
+    }, 0);
   });
 });
 // lnag
@@ -52,4 +76,3 @@ $(function () {
     }
   });
 });
-
